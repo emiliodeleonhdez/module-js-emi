c368e2cd1fd8161bcefcfcf6f9a92a77fb324312
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2021-09-14 18:08:24 -0500

    Subiendo ultimos cambios

---
+++ 08-objetos/js/execute.js
@@ -0,0 +1,108 @@
+/*
+- Obtener el score promedio de cada materia (HTML, CSS, JS)
+- Obtener el promedio indivual de cada mentor
+- Crear un array de string con la siguiente forma:
+  "Mi nombre es {name} y mi promedio es {promedio}"
+- Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+*/
+
+let mentorArray = [
+    {
+        name: "Angel Resendiz",
+        scores: [
+            {
+                signature:  "HTML",
+                score: 10
+            },
+            {
+                signature:  "CSS",
+                score: 10
+            },
+            {
+                signature:  "JS",
+                score: 9
+            },
+        ]
+    },
+    {
+        name: "Elda Corona",
+        scores: [
+            {
+                signature:  "HTML",
+                score: 10
+            },
+            {
+                signature:  "CSS",
+                score: 9
+            },
+            {
+                signature:  "JS",
+                score: 10
+            },
+        ]
+    },
+    {
+        name: "Alfred Altamirando",
+        scores: [
+            {
+                signature:  "HTML",
+                score: 9
+            },
+            {
+                signature:  "CSS",
+                score: 10
+            },
+            {
+                signature:  "JS",
+                score: 10
+            },
+        ]
+    },
+    {
+        name: "Tux Tuxtla",
+        scores: [
+            {
+                signature:  "HTML",
+                score: 10
+            },
+            {
+                signature:  "CSS",
+                score: 8
+            },
+            {
+                signature:  "JS",
+                score: 10
+            },
+        ]
+    },
+    {
+        name: "Fernanda Palacios",
+        scores: [
+            {
+                signature:  "HTML",
+                score: 10
+            },
+            {
+                signature:  "CSS",
+                score: 9
+            },
+            {
+                signature:  "JS",
+                score: 10
+            },
+        ]
+    }
+]
+
+
+
+/* console.log(mentorArray[0].scores[0].score) */
+
+console.log("La longitud de metonArray es " + mentorArray.length)
+
+
+
+for (let i=0; mentorArray.length; i++){
+    console.log("Esto es i " + i)
+    console.log(mentorArray[i].scores[i].score)
+}
---
+++ 08-objetos/js/main.js
@@ -0,0 +1,80 @@
+/* const personObject = {
+    name: "Emilio",
+    lastName: "De Leon",
+    gender: "Vato",
+    age: 28,
+    isSingle: true,
+    hobbies: [
+        "Salir de bici",
+        "Tomar vinito"
+    ]
+}
+console.log(personObject.lastName)
+
+console.log(personObject["hobbies"])
+
+personObject.birthDate = "07/07/1993"
+
+console.log(personObject.birthDate + personObject.name)
+personObject.zodiac = "Cancer"
+
+console.log(personObject.zodiac)
+
+personObject.hobbies.push("Ver series")
+
+console.log(personObject.hobbies)
+
+let nameUpper = personObject.name.toUpperCase()
+
+personObject.name = personObject.name.toUpperCase()
+
+console.log(personObject)
+
+
+let {name, lastName,age} = personObject
+
+console.log(name)
+console.log(lastName)
+console.log(age)
+
+console.log("----------")
+
+let firstName = "Emi"
+let generation = 13
+let bootcamp = "JavaScript"
+
+const koderObject = {
+    firstName,
+    generation,
+    bootcamp
+}
+console.log(koderObject) */
+/* 
+let dataArray = [
+    ["Fernanda", "Palacios"],
+    ["Alfred", "Altamirano"],
+    ["Angel", "Resendiz"],
+    ["Elda", "Corona"],
+    ["Tux", "Tuxtla"],
+    ["Jorge", "De Buen"]
+]
+
+let arrayMentor = [
+    {
+        name: dataArray[0][0],
+        lastName: dataArray[0][1]
+    },
+    {
+        name: dataArray[1][0],
+        lastName: dataArray[1][1]
+    },
+    {
+        name: dataArray[2][0],
+        lastName: dataArray[2][1]
+    }
+]
+
+console.log(arrayMentor) */
+
+// ArrayNameforEach(callback(item,index,array))
+
